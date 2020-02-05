870e5a94b34706b351d6607d959322deb0c927c5
radville <laura.radville@gmail.com>
2020-02-05 11:48:02 -0500

    changed WebMapView component to use esri-library

--- src/components/WebMapView.js
+++ src/components/WebMapView.js
@@ -1,34 +1,36 @@
-// Adapted from "Using the ArcGIS API for JavaScript with React"
+// From "Using the ArcGIS API for JavaScript with React"
 // found here: https://developers.arcgis.com/javascript/latest/guide/react/
 
 import React, { useEffect, useRef } from 'react';
-import ArcGISMap from 'esri/Map';
-import MapView from 'esri/views/MapView';
+import { loadModules } from 'esri-loader';
 
 export const WebMapView = () => {
     const mapRef = useRef();
-    
-    // create map when component is rendered
+
     useEffect(
       () => {
-        const map = new ArcGISMap({
-          basemap: 'topo-vector'
-        });
-    
-        // load the map view at the ref's DOM node
-        const view = new MapView({
-          container: mapRef.current,
-          map: map,
-          center: [-118, 34],
-          zoom: 8
-        });
+        // lazy load the required ArcGIS API for JavaScript modules and CSS
+        loadModules(['esri/Map', 'esri/views/MapView'], { css: true })
+        .then(([ArcGISMap, MapView]) => {
+          const map = new ArcGISMap({
+            basemap: 'topo-vector'
+          });
 
-        return () => {
-          if (view) {
-            // destroy the map view
-            view.container = null;
-          }
-        };
+          // load the map view at the ref's DOM node
+          const view = new MapView({
+            container: mapRef.current,
+            map: map,
+            center: [-118, 34],
+            zoom: 8
+          });
+
+          return () => {
+            if (view) {
+              // destroy the map view
+              view.container = null;
+            }
+          };
+        });
       }
     );
 
